ace40216c8ce82652646016503d29313381748ae
WillChrist007 <williamchristians007@gmail.com>
2022-11-03 21:00:57 +0700

    QR Scan [belom jadi]

--- app/src/main/java/com/william/etanodv2/MainActivity.kt
+++ app/src/main/java/com/william/etanodv2/MainActivity.kt
@@ -3,12 +3,15 @@ package com.william.etanodv2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.william.etanodv2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     var binding : ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
--- app/src/main/java/com/william/etanodv2/fragment/FragmentDonate.kt
+++ app/src/main/java/com/william/etanodv2/fragment/FragmentDonate.kt
@@ -1,15 +1,22 @@
 package com.william.etanodv2.fragment
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.william.etanodv2.EditProfileActivity
 import com.william.etanodv2.R
 import com.william.etanodv2.RVDonasiAdapter
+import com.william.etanodv2.ScanQRActivity
 import com.william.etanodv2.entity.donate
+import com.william.etanodv2.room.Constant
+import kotlinx.android.synthetic.main.fragment_donate.*
 
 class FragmentDonate : Fragment() {
     override fun onCreateView(
@@ -21,6 +28,9 @@ class FragmentDonate : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?){
         super.onViewCreated(view, savedInstanceState)
+
+        setupListener()
+
         val layoutManager = LinearLayoutManager(context)
         val adapter : RVDonasiAdapter = RVDonasiAdapter(donate.listOfDonate)
 
@@ -31,5 +41,13 @@ class FragmentDonate : Fragment() {
         rvDonate.setHasFixedSize(true)
 
         rvDonate.adapter =adapter
+    }
+
+    private fun setupListener(){
+        imgScan.setOnClickListener {
+            startActivity(
+                Intent(requireActivity().applicationContext, ScanQRActivity::class.java)
+            )
+        }
     }
 }
