1b175ca331e155a377efb72260d666c01f746368
Giovanni040204 <111994851+Giovanni040204@users.noreply.github.com>
2022-11-22 22:26:15 +0700

    Memperbaiki Database

--- app/src/main/java/com/william/etanodv2/api/UserApi.kt
+++ app/src/main/java/com/william/etanodv2/api/UserApi.kt
@@ -2,7 +2,7 @@ package com.william.etanodv2.api
 
 class UserApi {
     companion object {
-        val BASE_URL = "https://637791e00992902a250bd160.mockapi.io/api/"
+        val BASE_URL = "http://192.168.18.11/ci4-apiserver/public/"
 
         val GET_ALL_URL = BASE_URL + "user/"
         val GET_BY_ID_URL = BASE_URL + "user/"
