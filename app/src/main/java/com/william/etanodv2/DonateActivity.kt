78291828bdc98271b99b2bf6fbba95c7f8119e25
Giovanni040204 <111994851+Giovanni040204@users.noreply.github.com>
2022-11-27 16:28:58 +0700

    Membuat Vega Layout Manager (belum jadi)

--- app/src/main/java/com/william/etanodv2/DonateActivity.kt
+++ app/src/main/java/com/william/etanodv2/DonateActivity.kt
@@ -1,13 +1,13 @@
 package com.william.etanodv2
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import android.widget.LinearLayout
 import android.widget.SearchView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
